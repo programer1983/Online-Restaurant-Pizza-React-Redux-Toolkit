08bb8b69384e9335ebfa279b5ba052b39e404415
programer1983 <programer972@gmail.com>
2022-06-15 11:00:13 +0300

    global connection of routings, creation of new pages, breaking into components in pages;

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import "./scss/app.scss"
 import React from 'react'
 import Header from './components/Header'
-import Categories from './components/Categories'
-import Sort from './components/Sort'
-import PizzaBlock from './components/PizzaBlock'
-import Sceleton from './components/PizzaBlock/Sceleton'
-
-const API_URL = "https://62a6f83cbedc4ca6d7be4b30.mockapi.io/items"
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound"
 
 function App() {
-  const [items, setItems] = React.useState([])
-  const [loading, setIsLoading] = React.useState(true)
-
-  React.useEffect(() => {
-    fetch(`${API_URL}`)
-    .then((res) => {return res.json()})
-    .then((arr) => {
-      setItems(arr)
-      setIsLoading(false)
-    })
-  }, [])
+ 
   
   return (
         <div className="wrapper">
           <Header />
           <div className="content">
             <div className="container">
-              <div className="content__top">
-                <Categories />
-                <Sort />
-              </div>
-              <h2 className="content__title">Все пиццы</h2>
-              <div className="content__items">
-                {loading 
-                  ? [...new Array(6)].map((_, index) => <Sceleton key={index}/>) 
-                  : items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-              </div>
+              {/* <Home /> */}
+              <NotFound />
             </div>
           </div>
         </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./NotFoundBlock.module.scss"
+
+console.log(styles)
+
+const index = () => {
+  return (
+    <div>
+        <h1>
+          <span>😓 </span>
+          <br />
+           Ничего не найдено 
+        </h1>
+    </div>
+  )
+}
+
+export default index
