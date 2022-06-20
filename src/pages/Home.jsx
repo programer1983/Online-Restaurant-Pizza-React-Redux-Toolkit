e8d4d9264a7e87461f4ce1394f8f1dc2e129e808
programer1983 <programer972@gmail.com>
2022-06-20 16:49:24 +0300

    create pagination;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,13 @@ import Categories from './../components/Categories'
 import Sort from './../components/Sort'
 import PizzaBlock from './../components/PizzaBlock'
 import Sceleton from './../components/PizzaBlock/Sceleton'
-
-// const API_URL = "https://62a6f83cbedc4ca6d7be4b30.mockapi.io/items"
-
+import Pagination from '../components/Pagination'
 
 const Home = ({searchValue}) => {
     const [items, setItems] = React.useState([])
     const [loading, setIsLoading] = React.useState(true)
     const [categoryId, setCategoryId] = React.useState(0)
+    const [currentPage, setCurrentPage] = React.useState(1)
     const [sortType, setSortType] = React.useState({
       name: "Популярности",
       sortProperty: "rating"
@@ -23,14 +22,14 @@ const Home = ({searchValue}) => {
       const category = categoryId > 0 ? `category=${categoryId}` : ""
       const search = searchValue ? `&search=${searchValue}` : ""
       
-      fetch(`https://62a6f83cbedc4ca6d7be4b30.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search}`)
+      fetch(`https://62a6f83cbedc4ca6d7be4b30.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr)
         setIsLoading(false)
       })
       window.scrollTo(0, 0)
-    }, [categoryId, sortType, searchValue])
+    }, [categoryId, sortType, searchValue, currentPage])
 
     const pizzas = items.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)
     const sceletons = [...new Array(6)].map((_, index) => <Sceleton key={index}/>)
@@ -43,9 +42,8 @@ const Home = ({searchValue}) => {
             <Sort value={sortType} onChangeSort={(i) => setSortType(i)}/>
         </div>
         <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-            {loading ? sceletons : pizzas}
-        </div>
+        <div className="content__items">{loading ? sceletons : pizzas}</div>
+        <Pagination onChangePage={(number) => setCurrentPage(number)}/>
     </div>
   )
 }
